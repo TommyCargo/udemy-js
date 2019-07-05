e507d20fcfb7bb8eaf3b3778da934cb93423955a
Nick <s0fk@ya.ru>
2019-07-05 16:14:14 +0300

    1st task

---
+++ 1/script1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses:{},
+    optionalExpenses:{},
+    income: [],
+    savings: false
+};
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let answer2 = +prompt("Во сколько обойдется?", "");
+let answer3 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let answer4 = +prompt("Во сколько обойдется?", "");
+
+let expenses = {
+    answer1 : answer2,
+    answer3 : answer4
+};
+
+alert(appData.budget/30);
