6ecba1dbe85229da7d03fc0b3e3add260114bd85
Nickolai Chernyi <s0fk@ya.ru>
2019-07-20 22:07:29 +0300

    homeWork #13

--- es6.js
+++ es6.js
@@ -11,7 +11,7 @@ function makeArray() {
     var items = [];
     //for (var i = 0; i < 10; i++) {
     for (let i = 0; i < 10; i++) {
-        var item = function() {
+        var item = function () {
             console.log(i);
         };
         items.push(item);
@@ -33,7 +33,7 @@ fun();
 
 let obj = {
     number: 5,
-    sayNumber: function() {
+    sayNumber: function () {
         let say = () => {
             console.log(this);
         };
@@ -44,7 +44,7 @@ obj.sayNumber();
 
 let btn = document.querySelector('button');
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
     let show = () => {
         console.log(this);
     };
@@ -83,8 +83,82 @@ function log(a, b, c) {
     console.log(a);
     console.log(b);
     console.log(c);
-    console.log(a + b +c);
+    console.log(a + b + c);
 }
 let numbers = [2, 5, 7];
 
 log(...numbers);
+
+
+// Promise
+/*
+function shoot(arrow, headshot, fail) {
+    console.log('Вы сделали выстрел...');
+
+    setTimeout(function() {
+        Math.random() > 0.5 ? headshot({}) : fail('Вы промахнулись');
+
+    }, 3000);
+};
+
+function win() {
+    console.log('Вы победили!');
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer() {
+    console.log('Вам купили пиво!');
+}
+
+function giveMoney() {
+    console.log('Вам заплатили!');
+}
+
+function loose() {
+    console.log('Вы проиграли!');
+}
+let drink = 0;
+shoot({}, 
+        function(mark) {
+            console.log('Вы попали в цель');
+            win(mark, buyBeer, giveMoney);
+        }, 
+        function(miss) {
+            console.error(miss);
+            loose();
+        }
+    )
+*/
+function shoot(arrow) {
+    console.log('Вы сделали выстрел...');
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(function () {
+            Math.random() > 0.5 ? resolve({}) : reject('Вы промахнулись');
+    
+        }, 3000);
+    });
+    return promise;
+};
+
+function win() {
+    console.log('Вы победили!');
+    (drink == 1) ? buyBeer(): giveMoney();
+}
+
+function buyBeer() {
+    console.log('Вам купили пиво!');
+}
+
+function giveMoney() {
+    console.log('Вам заплатили!');
+}
+
+function loose() {
+    console.log('Вы проиграли!');
+}
+let drink = 0;
+
+shoot({})
+        .then(mark => console.log('Вы попали в цель'))
+        .then(win)
+        .catch(loose)
--- yoga/js/script.js
+++ yoga/js/script.js
@@ -78,3 +78,53 @@ for (index = 0; index < showMore.length; index++) {
     button = showMore[index];
     button.addEventListener('click', showModal);
 }
+
+// Form
+
+let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div'),
+    formDown = document.querySelector('#form');
+
+statusMessage.classList.add('status');
+
+form.addEventListener('submit', function (e) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    //request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    let formData = new FormData(form);
+
+    let obj = {};
+    formData.forEach(function(value, key) {
+        obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+    //request.send(formData);
+    request.send(json);
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if (request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success;
+        } else {
+            statusMessage.innerHTML = message.failure;
+        }
+    });
+
+    for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
+    }
+
+});
